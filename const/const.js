c72e1745e7a6d00edda2c736b0bdd11bf8030c9b
Stanislav Sorochan <stas.sorochan@gmail.com>
2023-03-02 22:38:45 +0100

    new port config

--- const/const.js
+++ const/const.js
@@ -1,5 +1,5 @@
 module.exports.REG_LINK = /^(https?:\/\/)(www\.)?([\w-.~:/?#[\]@!$&')(*+,;=]*\.?)*\.{1}[\w]{2,8}(\/([\w-.~:/?#[\]@!$&')(*+,;=])*)?/;
 
 module.exports.MONGO_DEV = 'mongodb://localhost:27017/moviesdb';
-module.exports.PORT_DEV = '3001';
+module.exports.PORT_DEV = '3000';
 module.exports.JWT_SECRET_DEV = 'dev-secret';
